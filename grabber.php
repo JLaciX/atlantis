e6a01fc876a948a95a05aa1a09a08eee925f0bb3
Drakxon <42521744+JLaciX@users.noreply.github.com>
2021-03-26 01:55:50 +0100

    x

--- grabber.php
+++ grabber.php
@@ -3,4 +3,7 @@ $content = file_get_contents($_POST['x']);
 //$content = str_replace(['src="/',"src='/"],['src="'.$_POST['x'],"src='".$_POST['x']],$content);
 //$content = str_replace(['src="/',"src='/"],['src="'.$_POST['x'],"src='".$_POST['x']],$content);
 $content = str_replace(['href="/',"href='/"],['href="'.$_POST['x'],"href='".$_POST['x']],$content);
+$content = str_replace(['script',"script"],['p style="display:none"',"p style='color:green'"],$content);
+//$pattern = "/<script ?.*>(.*)<\/script>/";
+//preg_replace($pattern, 'W3Schools', $content);
 echo $content;?>
