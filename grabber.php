82534bf45da897bb7103f533c210a1249f5e7a5b
Drakxon <42521744+JLaciX@users.noreply.github.com>
2021-03-28 20:46:42 +0200

    url

--- grabber.php
+++ grabber.php
@@ -1,8 +1,13 @@
 <?php 
 $content = file_get_contents($_POST['x']); 
+$url = parse_url($_POST['x'], PHP_URL_HOST);
+$protocol = parse_url($_POST['x'], PHP_URL_SCHEME); 
+$full = $protocol."://".$url."/";
+//var_dump( parse_url("https://www.pizzatabor.hu/#kinalatunk"));
+//echo $_POST['x'];
 //$content = str_replace(['src="/',"src='/"],['src="'.$_POST['x'],"src='".$_POST['x']],$content);
-//$content = str_replace(['src="/',"src='/"],['src="'.$_POST['x'],"src='".$_POST['x']],$content);
-$content = str_replace(['href="/',"href='/"],['href="'.$_POST['x'],"href='".$_POST['x']],$content);
+$content = str_replace(['src="/',"src='/"],['src="'.$_POST['x'],"src='".$_POST['x']],$content);
+$content = str_replace(['href="/',"href='/"],['href="'.$full,"href='".$full],$content);
 $content = str_replace(['script',"script"],['p style="display:none"',"p style='color:green'"],$content);
 //$pattern = "/<script ?.*>(.*)<\/script>/";
 //preg_replace($pattern, 'W3Schools', $content);
