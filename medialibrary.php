7e63dbe351fe353022ec3b7faf4ed007f40bb819
Drakxon <42521744+JLaciX@users.noreply.github.com>
2021-03-29 08:14:50 +0200

    végső design 3. szakasz

--- medialibrary.php
+++ medialibrary.php
@@ -3,5 +3,5 @@
   $files1 = scandir($dir);
  
   for ($i=2; $i <count( $files1); $i++) { 
-     echo('<img class="img-fluid widget-ico close" src="uploads/'.$files1[$i].'" alt="">');
+     echo('<div class="col-3"><img class="img-fluid close" src="uploads/'.$files1[$i].'" alt=""></div>');
   }
