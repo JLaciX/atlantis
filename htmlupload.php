a60854cc6ba3064e19783032a4ce16628e158407
Drakxon <42521744+JLaciX@users.noreply.github.com>
2021-04-10 02:10:04 +0200

    w

--- htmlupload.php
+++ htmlupload.php
@@ -2,21 +2,21 @@
 
 if(isset($_FILES['file']['name'])){
 
-   /* Getting file name */
+   
    $filename = $_FILES['file']['name'];
 
-   /* Location */
+   
    $location = "templates/".$filename;
    $imageFileType = pathinfo($location,PATHINFO_EXTENSION);
    $imageFileType = strtolower($imageFileType);
 
-   /* Valid extensions */
+   
    $valid_extensions = array("html","css","php");
 
    $response = 0;
-   /* Check file extension */
+   
    if(in_array(strtolower($imageFileType), $valid_extensions)) {
-      /* Upload file */
+     
       if(move_uploaded_file($_FILES['file']['tmp_name'],$location)){
          $response = $location;
       }
